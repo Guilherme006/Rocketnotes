71b2ca75cb73a1c0767cb92dfce50eafc09fe289
Guilherme006 <137656860+Guilherme006@users.noreply.github.com>
2024-06-14 19:22:47 -0300

    Update app.routes.jsx

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -11,7 +11,7 @@ export function AppRoutes() {
 
         <Routes>
 
-            <Route path='/' element={<Home/>}/>
+            <Route path='/home' element={<Home/>}/>
             <Route path='/new' element={<New/>}/>
             <Route path='/profile' element={<Profile/>}/>
             <Route path='/details/:id' element={<Details/>}/>
